da224560130b9025786fc0c92dfface499010218
Jakub Dębski <hello@dbski.pl>
2024-11-10 22:18:53 +0100

    Fix types

--- vitest-setup.ts
+++ vitest-setup.ts
@@ -1,8 +1,1 @@
 import "@testing-library/jest-dom/vitest";
-import "@testing-library/jest-dom";
-
-import { type TestingLibraryMatchers } from "@testing-library/jest-dom/matchers";
-
-declare module "vitest" {
-  type Assertion<T = unknown> = {} & TestingLibraryMatchers<T, void>;
-}
